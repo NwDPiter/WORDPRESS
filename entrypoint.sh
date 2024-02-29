c946c311c2a754338c0379ff7a4fcd28f8470365
Pedro_Sales <69434231+NwDPiter@users.noreply.github.com>
2024-02-29 19:17:39 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ until mysqladmin ping -h"db" --silent; do
 done
 
 # Ativa o plugin Tainacan
-wp plugin activate tainacan --allow-root --path=/var/www/html
+wp plugin activate tainacan --allow-root --path=./tainacan
 
 # Continua com o comando original do Docker
 exec "$@"
